2aa659f499484d325922a238183a31642ad14149
debjyotip-nd <debjyoti.paul9007@gmail.com>
2019-02-12 19:15:46 +0530

    port changed

--- server.js
+++ server.js
@@ -9,8 +9,13 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 app.use('/recipebook', recipebook);
 
-let port = 8080;
 
-app.listen(port, () => {
-    console.log('Server is up and running on port numner ' + port);
+var server = app.listen(process.env.PORT || 5000, function () {
+  var port = server.address().port;
+  console.log("Express is working on port " + port);
 });
+// let port = 8080;
+
+// app.listen(port, () => {
+//     console.log('Server is up and running on port numner ' + port);
+// });
